9d3070755bae44a6efd90429166b48fa3b121be4
YeonJangL <lej32552002@gmail.com>
2025-06-09 21:23:39 +0900

    약 아이템 습득 스크립트 수정

--- CRAZYMAN/Assets/Scripts/Item/ItemDataForInventory.cs
+++ CRAZYMAN/Assets/Scripts/Item/ItemDataForInventory.cs
@@ -7,16 +7,27 @@ public class ItemDataForInventory
     public string itemName; // 아이템 이름
     public Sprite inventoryIcon; // 인벤토리 UI 아이콘
 
+    public float RecoveryStamina; // 스테미너 회복량
+    public float RecoveryMental; // 정신력 회복량
+
     public ItemDataForInventory(Item item)
     {
         if (item == null)
         {
             Debug.LogError("ItemDataForInventory 생성 오류: 전달된 Item 컴포넌트가 null입니다!");
+            this.itemType = ItemType.pills; // 또는 ItemType.Default 등 기본값
+            this.itemName = "Null Item";
+            this.inventoryIcon = null;
+            this.RecoveryStamina = 0f;
+            this.RecoveryMental = 0f;
             return; // null이면 초기화 실패
         }
 
         this.itemType = item.itemType;
         this.itemName = item.itemName;
         this.inventoryIcon = item.inventoryIcon;
+
+        this.RecoveryStamina = item.staminaRecoveryAmount;
+        this.RecoveryMental = item.mentalRecoveryAmount;
     }
 }
